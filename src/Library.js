1c62aa421731bd01d5e8bf07efc43468061b984f
Brendan Manning <tuj97064@temple.edu>
2018-07-02 21:23:43 -0400

    adding all() & download()

--- src/Library.js
+++ src/Library.js
@@ -1,15 +1,32 @@
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+
+const ytdl = require('ytdl-core');
 var fs = require('fs');
 
-
+/**
+ * Library class manages songs and metadata stored in the /data/music folder
+ */
 class Library {
     
-
-    static all() {
+    static async all() {
         
-        fs.readdir(__dirname + '/data/music/', function(err, items) {       
+        fs.readdir(__dirname + '/data/music/', function(err, items) { 
+            
+            var outputObject = {};
+
             for (var i=0; i<items.length; i++) {
-                console.log(items[i]);
+                let item = items[i];
+
+                console.log(item);
             }
         });
     } 
+
+    static async download(id, title, artist, album) {
+        await ytdl("https://www.youtube.com/watch?v=" + id, { quality: 'highestaudio', filter: 'audioonly' }).pipe(fs.createWriteStream(id + '.mp3'));
+        console.log("Done....");
+    }
 }
+
+module.exports = Library;
